750c739d85a6cfaee31fdfbef4a4918f64f9bf4f
Gsakura42456 <2473847852@qq.com>
2026-01-24 18:22:48 +0800

    ?

--- source/states/substates/PauseSubState.hx
+++ source/states/substates/PauseSubState.hx
@@ -147,7 +147,7 @@ class PauseSubState extends MusicBeatSubstate
 		add(grpMenuShit);
 
 		regenMenu();
-		cameras = [FlxG.cameras.list[FlxG.cameras.list.length - 1];
+		cameras = [FlxG.cameras.list[FlxG.cameras.list.length - 1]];
 	}
 
 	var holdTime:Float = 0;
